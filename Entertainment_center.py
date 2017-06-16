096c6b4e1a64e107d9f192f9c1826bc3be32e9a1
flex2016 <reloded2004@hotmail.com>
2017-06-16 12:43:26 -0400

    Add files via upload

---
+++ Entertainment_center.py
@@ -0,0 +1,30 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story", "A Story of a boy and his toys.",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=NBepTulmSMw")
+#print(toy_story.storyline)
+#print(toy_story.poster_image_url)
+
+avatar = media.Movie ("Avatar", "A marine on an alien planet",
+                      "http://upload.wikimedia.org/wikimedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                      "http://www.youtube.com/watch?v=-ceBgWV8io")
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+justice_league = media.Movie ("Justice League", "A superhero Movie",
+                      "https://upload.wikimedia.org/wikipedia/commons/8/8b/Justice_League_2017_film_logo.jpg",
+                      "https://www.youtube.com/watch?v=RWCQ22JyCL4")
+#justice_league.show_trailer()
+
+movies = [toy_story, avatar, justice_league]
+
+fresh_tomatoes.open_movies_page(movies)
+
+#print(media.Movie.VALID_RATINGS)
+#print(media.Movie.__doc__)
+#print(media.Movie.__name__)
+print(media.__name__)
+print(__name__)
+print(__module__)
